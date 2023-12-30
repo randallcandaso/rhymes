c2c9b7b9dc216d142a2f379e31f5125fd43aecca
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:46:27 -0700

    Update rhymes.py

--- rhymes.py
+++ rhymes.py
@@ -1,15 +1,3 @@
-'''
-
-File: rhymes.py
-Author: Randall Candaso
-Course/Section: CSC 120-002
-Purpose: This program takes both an inputted word and an inputted 
-dictionary, determining which words in the dictionary rhyme with
-the inputted one. This is determined by the splitting of syllables/
-pronunciations of all the words contained in the dictionary.
-
-'''
-
 def main():
     
     pfile = input()
